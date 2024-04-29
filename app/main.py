e6d8265bc2b3016fc5ebf2fb4e0ccca6d318ddb4
Mykola Kokoiev <veokoknik@gmail.com>
2024-04-29 23:17:21 +0200

    SOL

--- app/main.py
+++ app/main.py
@@ -2,39 +2,37 @@ import json
 from decimal import Decimal
 
 
-def calculate_profit(file: str) -> None:
+def calculate_profit(filename: str) -> None:
     result = {
         "earned_money": "0",
         "matecoin_account": "0"
     }
 
-    with open(file, "r") as file:
-        trades = json.load(file)
+    with open(filename, "r") as filename:
+        trades = json.load(filename)
 
     for contract in trades:
         if contract["bought"]:
             result["matecoin_account"] = str(
-                Decimal(result["matecoin_account"]) +
-                Decimal(contract["bought"])
+                Decimal(result["matecoin_account"])
+                + Decimal(contract["bought"])
             )
             result["earned_money"] = str(
-                Decimal(result["earned_money"]) -
-                Decimal(contract["bought"]) *
-                Decimal(contract["matecoin_price"])
+                Decimal(result["earned_money"])
+                - Decimal(contract["bought"])
+                * Decimal(contract["matecoin_price"])
             )
 
         if contract["sold"]:
             result["matecoin_account"] = str(
-                Decimal(result["matecoin_account"]) -
-                Decimal(contract["sold"])
+                Decimal(result["matecoin_account"])
+                - Decimal(contract["sold"])
             )
             result["earned_money"] = str(
-                Decimal(result["earned_money"]) +
-                Decimal(contract["sold"]) *
-                Decimal(contract["matecoin_price"])
+                Decimal(result["earned_money"])
+                + Decimal(contract["sold"])
+                * Decimal(contract["matecoin_price"])
             )
 
-    with open("profit.json", "w") as file:
-        json.dump(result, file, indent=2)
-
-
+    with open("profit.json", "w") as filename:
+        json.dump(result, filename, indent=2)
